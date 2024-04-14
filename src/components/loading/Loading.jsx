b679dd1586ce60289285f2d52b90935bd79b2a20
SimaGhoreyshi <simaghoreyshi1996@gmail.com>
2024-04-14 18:21:02 +0330

    feat: added loading component

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,10 @@
+import "./loading.scss";
+import { LoadingIcon } from "../../assets/icons";
+
+export const Loading = () => {
+  return (
+    <div className="loading">
+      <LoadingIcon />
+    </div>
+  );
+};
