e92ec165680c82f08d222870c9a28bf152331009
SimaGhoreyshi <simaghoreyshi1996@gmail.com>
2024-04-14 18:23:02 +0330

    feat: added header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import { FullLogo } from "../../assets/icons";
+import "./header.scss";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <FullLogo />
+    </div>
+  );
+};
